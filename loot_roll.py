33a26495223daa40f595f2f2337c9652c40b9474
Jon V <168088760+inkvert@users.noreply.github.com>
2024-11-23 17:04:28 +0000

    Update loot_roll.py
    Added the generation of a random dragon, which once defeated grants the item. Also added a random amount of gold dropped once the monster is defeated.

--- loot_roll.py
+++ loot_roll.py
@@ -10,7 +10,7 @@ Current notes:
 import random
 
 #Words which are placed at random at the start of item names, e.g BARBED Blade of the Tiger (currently 10 prefixes)
-PREFIXES = [
+SWORD_PREFIXES_1H = [
 "Barbed",
 "Vorpal",
 "Keen",
@@ -38,7 +38,7 @@ SWORD_NAMES_1H = [
 ]
 
 #Words which are placed at random at the end of item names, e.g Barbed Blade of the TIGER (currently 10 suffixes)
-SUFFIXES = [
+SWORD_SUFFIXES_1H = [
 "Tiger",
 "Wolf",
 "Eagle",
@@ -51,14 +51,44 @@ SUFFIXES = [
 "Viper"
 ]
 
+#Words which are placed at the start of dragon monster name, e.g ELDER Volcano Dragon (currently 5 ages)
+DRAGON_AGE = [
+"Hatchling",
+"Juvenile",
+"Mature",
+"Elder",
+"Ancient",
+]
+
+#Words which are placed after the age in the dragon monster namer, e.g Elder VOLCANO Dragon (currently 10 prefixes)
+DRAGON_PREFIXES = [
+"Volcano",
+"Thunder",
+"Frozen",
+"Tempest",
+"Mountain",
+"Arcane",
+"Shadow",
+"Eldritch",
+]
+
+
 def main():
 
 #Pulls a random prefix from PREFIXES
-    random_prefix = random.choice(PREFIXES)
+    random_prefix = random.choice(SWORD_PREFIXES_1H)
 #Pulls a random one-handed sword name from SWORD_NAMES_1H
     random_sword1h = random.choice(SWORD_NAMES_1H)
 #Pulls a random suffix from SUFFIXES
-    random_suffix = random.choice(SUFFIXES)
+    random_suffix = random.choice(SWORD_SUFFIXES_1H)
+
+#Pulls a random dragon age from DRAGON_AGE
+    random_dragon_age = random.choice(DRAGON_AGE)
+#Pulls a random dragon prefix from DRAGON_PREFIXES
+    random_dragon_prefix = random.choice(DRAGON_PREFIXES)
+
+#Calculates the the level of the monster
+    monster_level = random.randint(80, 110)
 
 #Generates an item level (currently does nothing)
     item_level = random.randint(80, 110)
@@ -72,15 +102,24 @@ def main():
     raw_dps = ((minimum_damage+maximum_damage)/2)/attack_speed
 #Calculates DPS adjusted by a percentage informed by item_level
     total_dps = round(((raw_dps/100)*item_level),2)
+
 #Randomly rolls an item name, formatted as an f-string
     item_name = (f'{random_prefix} {random_sword1h} of the {random_suffix}')
+#Randomly rolls a dragon name, formatted as an f-string
+    dragon_name = (f'{random_dragon_age} {random_dragon_prefix} Dragon')
+#Calculates a random amount of gold dropped by the enemy, scaled by monster_level
+    gold_dropped = (random.randint(50, 500)*item_level)
+
+#Prints the name and level of the monster in the console
+    print(end='\n')
+    print("You have defeated the Level", monster_level, dragon_name)
+    print(end='\n')
 
 #Prints the generated weapon in the console
     print("Congratulations! You receive:")
     print(end='\n')
     print(item_name)
     print("Item level:", item_level)
-    print(end='\n')
     print("Damage:", minimum_damage, "-", maximum_damage)
     print("Attack speed:", attack_speed)
     print("DPS:", total_dps)
@@ -101,6 +140,11 @@ def main():
         print("S tier: Wow! An incredible weapon, and a rare find.")
     if total_dps >= 160:
         print("S+ tier: Legendary weapon! It doesn't get any better than this.")
+
+    # Prints a random amount of gold earned in the console
+    print(end='\n')
+    print("...you also receive", gold_dropped, "gold pieces.")
+    print(end='\n')
 
 if __name__ == "__main__":
     main()
